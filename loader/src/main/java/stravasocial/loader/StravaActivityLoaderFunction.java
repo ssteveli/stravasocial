954dea31e3c4eac7d3630bb294434b4f638d3456
Scott Stevelinck <scott@stevelinck.com>
2014-07-23 19:33:57 -0600

    started implementation

---
+++ loader/src/main/java/stravasocial/loader/StravaActivityLoaderFunction.java
@@ -0,0 +1,17 @@
+package stravasocial.loader;
+
+import org.gearman.client.GearmanJobResult;
+import org.gearman.worker.AbstractGearmanFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StravaActivityLoaderFunction extends AbstractGearmanFunction {
+	private static Logger log = LoggerFactory.getLogger(StravaActivityLoaderFunction.class);
+	
+	@Override
+	public GearmanJobResult executeFunction() {
+	
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
