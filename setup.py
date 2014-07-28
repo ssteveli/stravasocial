8e4d33869eb0f0cbcd5999428d91442b954ead1a
Scott Stevelinck <scott@stevelinck.com>
2014-07-28 17:54:56 -0600

    new setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='stravasocial',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='ssteveli',
+    author_email='scott@stevelinck.com',
+    description=''
+)
