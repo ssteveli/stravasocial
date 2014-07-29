4dc6d116a4b0fa4e181852d793b1e8e63326a41a
Scott Stevelinck <scott@stevelinck.com>
2014-07-29 14:18:42 -0600

    working away

--- web/html/js/app.js
+++ web/html/js/app.js
@@ -22,13 +22,34 @@ app.config(['$routeProvider', '$locationProvider',
             when('/comparisons', {
                 templateUrl: 'comparisons.html',
                 controller: 'ComparisonController'
+            }).
+            when('/comparisons/:comparisonId', {
+                templateUrl: '/comparison-detail.html',
+                controller: 'ComparisonDetailController'
             });
 	}
 ]);
 
 angular.module('myFilters', []).filter('timeago', function() {
     return function(input) {
-        console.log('here we go!');
         return $.timeago(new Date(input));
     }
+}).filter('feet', function() {
+    return function(input) {
+        var i = parseFloat(input);
+        return (i * 0.000621371).toFixed(2) + 'mi';
+    }
+}).filter('seconds', function() {
+   return function(input) {
+       var sec_num = parseInt(input, 10); // don't forget the second param
+       var hours   = Math.floor(sec_num / 3600);
+       var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+       var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+       if (hours   < 10) {hours   = "0"+hours;}
+       if (minutes < 10) {minutes = "0"+minutes;}
+       if (seconds < 10) {seconds = "0"+seconds;}
+       var time    = hours+':'+minutes+':'+seconds;
+       return time;
+   }
 });
--- web/html/js/controllers.js
+++ web/html/js/controllers.js
@@ -3,7 +3,15 @@ var appControllers = angular.module('appControllers', ['ipCookie']);
 
 appControllers.controller('MainController', ['$scope', '$routeParams', '$http', 'ipCookie',
 	function($scope, $routeParams, $http, ipCookie) {
-		manageSession($scope, $http, ipCookie);
+        $scope.athlete = null;
+
+        $http.get('/api/strava/athlete').
+            success(function(data) {
+                $scope.athlete = data;
+            }).error(function(error) {
+                console.log('athlete resource error: ' + error);
+                $scope.athlete = null;
+            });
 
 		$scope.sendToStrava = function() {
 			return_url = window.location.protocol + 
@@ -20,7 +28,6 @@ appControllers.controller('MainController', ['$scope', '$routeParams', '$http', 
 		$scope.disconnect = function disconnect() {
 			$http.delete('/api/strava/authorizations/' + $scope.sessionId).
 				success(function(data) {
-					$scope.sessionIsValid = null;
 					$scope.athlete = null;
 					ipCookie.remove('stravaSocialSessionId');
 				});
@@ -28,28 +35,34 @@ appControllers.controller('MainController', ['$scope', '$routeParams', '$http', 
 	}
 ]);
 
-appControllers.controller('ComparisonController', ['$scope', '$http', '$routeParams', 'ipCookie',
-    function ($scope, $http, $routeParams, ipCookie) {
-        $scope.sessionId = ipCookie('stravaSocialSessionId');
-        $scope.comparisons = {};
+appControllers.controller('ComparisonController', ['$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
+        $scope.comparisons = [];
 
-        if ($scope.sessionId) {
-            console.log('validating session ' + $scope.sessionId);
-            $http.get('/api/strava/authorizations/' + $scope.sessionId + '/isvalid').
-                success(function(data) {
-                    $scope.sessionIsValid = data.is_valid;
+        $http.get('/api/strava/comparisons').
+            success(function(data) {
+                data.sort(compare);
+                $scope.comparisons = data;
+            });
 
-                    $http.get('/api/strava/athletes/' + data.athlete_id).
-                        success(function(data) {
-                            $scope.athlete = data;
+        function compare(a, b) {
+            if (a.started_ts < b.started_ts)
+                return 1;
+            if (a.started_ts > b.started_ts)
+                return -1;
 
-                            $http.get('/api/strava/athletes/' + data.id + '/comparisons').
-                                success(function(data) {
-                                    $scope.comparisons = data;
-                                });
-                        });
-                });
+            return 0;
         }
+    }]);
+
+appControllers.controller('ComparisonDetailController', ['$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
+        $scope.comparison = {};
+
+        $http.get('/api/strava/comparisons/' + $routeParams.comparisonId).
+            success(function (data) {
+                $scope.comparison = data;
+            });
     }]);
 
 function manageSession($scope, $http, ipCookie) {
