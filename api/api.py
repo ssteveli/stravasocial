3577f591b47dfc8c481775d3964a7b5ca96df2c8
Scott Stevelinck <scott@stevelinck.com>
2014-07-29 14:19:09 -0600

    reshaping the api to be simplier

--- api/api.py
+++ api/api.py
@@ -1,10 +1,9 @@
 #!/usr/local/bin/python
 
-from flask import Flask, jsonify, abort, make_response, Response
-from flask import request
+from flask import Flask, jsonify, abort, make_response, Response, request
 from strava import Strava
 from pymongo import MongoClient
-from bson.json_util import dumps
+from bson.json_util import dumps, ObjectId
 
 import json
 
@@ -16,17 +15,35 @@ comparisons = db.comparisons
 
 @app.errorhandler(404) 
 def not_found(error):
-    return make_response(jsonify({'error': 'not found'}), 404)
+    return make_response(jsonify({'error': 'not found', 'message': error.description}), 404)
 
-@app.route('/api/strava/athletes/<int:id>/comparisons')
-def getComparisons(id):
-    athlete = strava.getAthlete(id)
+@app.errorhandler(400)
+def invalid_request(error):
+    return make_response(jsonify({'error': 'invalid request', 'message': error.description}), 400)
 
-    if athlete is None:
-        abort(404)
+@app.route('/api/strava/comparisons')
+def getComparisonsBySession():
+    athlete = validateSessionAndGetAthlete()
+    return Response(dumps(comparisons.find({'athlete_id': athlete['id']})), mimetype='application/json')
 
-    print 'finding comparisons for athlete: {id}'.format(id = id)
-    return Response(dumps(comparisons.find({'athlete_id': id})), mimetype='application/json')
+@app.route('/api/strava/comparisons/<comparisonid>')
+def getComparisonBySession(comparisonid):
+    athlete = validateSessionAndGetAthlete()
+    try:
+        _id = ObjectId(str(comparisonid))
+        comparison = comparisons.find_one({'_id': ObjectId(str(comparisonid))})
+
+        if comparison is None or athlete['id'] != comparison['athlete_id']:
+            abort(404, 'the specified comparison id was not found')
+
+        return Response(dumps(comparison), mimetype='application/json')
+    except:
+        abort(400, 'invalid comparison id')
+
+@app.route('/api/strava/athlete')
+def getAthleteBySession():
+    athlete = validateSessionAndGetAthlete()
+    return Response(json.dumps(athlete), mimetype='application/json')
 
 @app.route('/api/strava/athletes/<id>')
 def getAthlete(id):
@@ -36,19 +53,6 @@ def getAthlete(id):
         abort(404)
     else:
         return Response(json.dumps(athlete), mimetype='application/json')
-
-@app.route('/api/strava/athletes/<id>/loadactivities', methods=['POST'])
-def loadAthleteActivites(id):
-    athlete = strava.getAthlete(id)
-     
-    if athlete is None:
-        abort(404)
-    else:
-        result = strava.startActivityLoad(id)
-        return json.dumps({
-            'id': result.state,
-            'state': result.state
-        })
     
 @app.route('/api/strava/authorization')
 def createAuthorization():
@@ -57,8 +61,6 @@ def createAuthorization():
 @app.route('/api/strava/authorizations/<id>', methods=['PUT'])
 def updateAuthentication(id):
     req_json = request.get_json()
-    print 'request: ' + str(req_json)
-    
     return json.dumps(strava.updateAuthorization(id, req_json['code']))
 
 @app.route('/api/strava/authorizations/<id>', methods=['DELETE'])
@@ -69,6 +71,30 @@ def deleteAuthentication(id):
 @app.route('/api/strava/authorizations/<id>/isvalid')
 def validSession(id):
     return json.dumps(strava.isAuthenticated(id))
+
+def validateSessionAndGetAthlete():
+    sessionId = request.cookies.get('stravaSocialSessionId')
+
+    if sessionId is None:
+        abort(401, 'no authorization information received')
+
+    authorization = strava.getAuthorization(sessionId)
+
+    if authorization is None:
+        abort(401, 'missing authorization received')
+
+    if 'access_token' not in authorization:
+        abort(403, 'authorization session is not complete, missing access token from Strava API')
+
+    if 'athlete_id' not in authorization:
+        abort(403, 'authorization session is not complete, missing athlete information')
+
+    athlete = strava.getAthlete(authorization['athlete_id'])
+
+    if athlete is None:
+        abort(404, 'athlete not found')
+
+    return athlete
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True)
