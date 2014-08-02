749db31c37c2fa23820145fcc4d975944228010b
Scott Stevelinck <scott@stevelinck.com>
2014-08-02 06:30:26 -0600

    added yum update

--- scripts/linode-base-setup.sh
+++ scripts/linode-base-setup.sh
@@ -1,5 +1,6 @@
 i#!/bin/bash
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+yum update -y
 yum install -y puppet git
 
 git clone https://github.com/ssteveli/puppet.git /opt/puppet
