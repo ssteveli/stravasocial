dc559d1566ebbf23954fcccdd0d5de57ec1d5056
Scott Stevelinck <scott@stevelinck.com>
2014-08-02 16:57:54 -0600

    added clone of the puppetlabs firewall module

--- scripts/linode-base-setup.sh
+++ scripts/linode-base-setup.sh
@@ -9,5 +9,11 @@ else
 	cd /opt/puppet; git pull
 fi
 
+if [ ! -d "/opt/puppet/modules/firewall" ]; then
+	git clone https://github.com/puppetlabs/puppetlabs-firewall.git /opt/puppet/modules/firewall
+else
+	cd /opt/puppet/modules; git pull
+fi
+
 puppet apply /opt/puppet/manifasts/site.pp --modulepath=/opt/puppet/modules
 
